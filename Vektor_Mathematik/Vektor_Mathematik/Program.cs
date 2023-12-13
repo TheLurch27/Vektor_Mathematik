3f9d85cf5d4ccc3d34255172af1153f0aef67993
TheLurch27 <info@sae.edu>
2023-12-13 11:44:41 +0100

    Added Visual Studio File

---
+++ Vektor_Mathematik/Vektor_Mathematik/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vektor_Mathematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
